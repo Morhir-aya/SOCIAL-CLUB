e5e589f0bd47bc0ca69ce653797a4b50731a0ebf
aya <aya.morhir.is@gmail.com>
2022-09-06 13:17:10 +0200

    666

--- static/js/actualite.js
+++ static/js/actualite.js
@@ -2,7 +2,7 @@
     let joinbtn = document.querySelector('#joinbtn');
     let input1 = document.querySelector('#input1');
     let input2 = document.querySelector('#input2');
-
+    
     likebtn.addEventListener('click',()=>{
         // input1.value = parseInt(input1.value)+1;
         likebtn.style.color = "rgb(59, 56, 56)";
